35f136a83fded0494941309143a7d95a22d7000a
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2025-10-07 16:28:51 +1100

    Updated Jest "utilities" test with additional test cases

--- tests/utilities.test.js
+++ tests/utilities.test.js
@@ -15,17 +15,61 @@ test("wrapString: wrap complex sentence", () => {
 // PLACE TESTS FOR isValidDateString UNDER HERE
 
 // Test for invalid string
+test("isValidDateString: check invalid string", () => {
+    expect(isValidDateString("not a date")).toBe(false);
+});
 
 // Test for the wrong amount of "date segments" (see comments in utilities.js for more info)
+test("isValidDateString: too few date segments", () => {
+    expect(isValidDateString("12/2025")).toBe(false);
+});
+
+test("isValidDateString: too many date segments", () => {
+    expect(isValidDateString("12/12/12/2025")).toBe(false);
+});
 
 // Test for wrong number of digits in the day
+test("isValidDateString: wrong number of digits in the day", () => {
+    expect(isValidDateString("123/12/2025")).toBe(false);
+});
+
+test("isValidDateString: wrong number of digits in the day", () => {
+    expect(isValidDateString("1/12/2025")).toBe(false);
+});
 
 // Test for wrong number of digits in the month
+test("isValidDateString: wrong number of digits in the month", () => {
+    expect(isValidDateString("12/123/2025")).toBe(false);
+});
+
+test("isValidDateString: wrong number of digits in the month", () => {
+    expect(isValidDateString("12/1/2025")).toBe(false);
+});
 
 // Test for wrong number of digits in the year
+test("isValidDateString: wrong number of digits in the year", () => {
+    expect(isValidDateString("12/12/02025")).toBe(false);
+});
 
 // Test for day outside of month's number of days
+test("isValidDateString: day outside of month's number of days", () => {
+    expect(isValidDateString("32/12/2025")).toBe(false);
+});
+
+test("isValidDateString: day outside of month's number of days", () => {
+    expect(isValidDateString("30/02/2025")).toBe(false);
+});
 
 // Test for month greater than 12
+test("isValidDateString: month greater than 12", () => {
+    expect(isValidDateString("12/13/2025")).toBe(false);
+});
 
 // Test for day <= 0
+test("isValidDateString: day <= 0", () => {
+    expect(isValidDateString("00/12/2025")).toBe(false);
+});
+
+test("isValidDateString: day <= 0", () => {
+    expect(isValidDateString("-1/12/2025")).toBe(false);
+});
